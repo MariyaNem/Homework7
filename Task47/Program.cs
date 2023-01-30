7401317e55c3e7ec75518078b2c58d0a5a648d4b
Mariya Nemzorova <nemzorova777@mail.ru>
2023-01-30 18:14:11 +0700

    Solve task 47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -4,3 +4,35 @@
 // 1   -3,3    8  -9,9
 // 8    7,8  -7,1   9
 
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            double num = rnd.NextDouble()*(max-min)+min;
+            matrix[i, j] = Math.Round(num,1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 5},");
+            else Console.Write($"{matrix[i, j], 5}  ");
+        }
+        Console.WriteLine("]");
+    }   
+}
+
+double[,] array2D = CreateMatrixRndDouble(3, 4, -10, 9);
+PrintMatrix(array2D);
